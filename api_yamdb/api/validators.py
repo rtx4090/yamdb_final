a239211f77117592bc41d9c94ccd3307c2181b5c
rtx4090 <95490984+rtx4090@users.noreply.github.com>
2022-07-05 21:06:36 +0700

    Delete api_yamdb directory

--- api_yamdb/api/validators.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-from rest_framework.exceptions import ValidationError
-
-
-def year_validator(value):
-    if value < 1 or value > datetime.now().year:
-        raise ValidationError(f'{value} - is not a correct year!')
